74f70dc0f2d4e253160e656b91a7f538f5b98350
luismenslin <luis.fmenslinx@gmail.com>
2024-06-10 13:15:29 -0300

    init

---
+++ src/main/java/br/com/univille/germoneys/GermoneysApplication.java
@@ -0,0 +1,13 @@
+package br.com.univille.germoneys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermoneysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermoneysApplication.class, args);
+	}
+
+}
