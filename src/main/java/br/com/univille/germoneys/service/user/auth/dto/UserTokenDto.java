c1d4be0ed607c9d017c019b5846a8e2aa649c783
Vinícius Wille <viniciuswillealves@gmail.com>
2024-06-23 00:50:15 -0300

    Using @Data

--- src/main/java/br/com/univille/germoneys/service/user/auth/dto/UserTokenDto.java
+++ src/main/java/br/com/univille/germoneys/service/user/auth/dto/UserTokenDto.java
@@ -1,5 +1,8 @@
 package br.com.univille.germoneys.service.user.auth.dto;
 
+import lombok.Data;
+
+@Data
 public class UserTokenDto {
 
     private Long userId;
@@ -9,36 +12,4 @@ public class UserTokenDto {
     private String email;
 
     private String token;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 }
