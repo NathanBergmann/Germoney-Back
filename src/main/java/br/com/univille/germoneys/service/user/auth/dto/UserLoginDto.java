cbfe411e039b354aed41c8509a7056d2d898f1d3
Vinícius Wille <viniciuswillealves@gmail.com>
2024-06-23 00:54:07 -0300

    Using @Data

--- src/main/java/br/com/univille/germoneys/service/user/auth/dto/UserLoginDto.java
+++ src/main/java/br/com/univille/germoneys/service/user/auth/dto/UserLoginDto.java
@@ -1,7 +1,9 @@
 package br.com.univille.germoneys.service.user.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
+@Data
 public class UserLoginDto {
 
     @Schema(description = "E-mail do usuário", example = "admin@germoneys.com")
@@ -9,20 +11,4 @@ public class UserLoginDto {
 
     @Schema(description = "Senha do usuário", example = "GermoneysAdmin22062024!")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/br/com/univille/germoneys/service/user/dto/UserCreationDto.java
+++ src/main/java/br/com/univille/germoneys/service/user/dto/UserCreationDto.java
@@ -3,7 +3,9 @@ package br.com.univille.germoneys.service.user.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
+@Data
 public class UserCreationDto {
 
     @Size(min = 3, max = 10)
@@ -17,28 +19,4 @@ public class UserCreationDto {
     @Size(min = 6, max = 20)
     @Schema(description = "Senha do usuário", example = "123456")
     private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
